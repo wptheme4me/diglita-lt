9e7d91caf7dd3d659d224250ffbae77c5e8d0878
unknown <hey@wptheme4.me>
2019-02-09 22:39:34 +0200

    config for netlify

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     site_title: "UAB DIGLITA - oficialus MICHELIN padangų atstovas Lietuvoje",
     dev_siteurl: "http://localhost:3000/",
-    prod_siteurl: "https://name.netlify.com/",
+    prod_siteurl: "https://diglita-lt.netlify.com/",
     prod_siteurl_fb_img: "https://diglita-lt.netlify.com/img/logo.png",
     google_analytics_key: "API-KEY",
     facebook_appid: "APP_ID",
